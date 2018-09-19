e246a74a0273d3a7d532ba74b287dc517c3ef6b4
JanDeveloper <sojak@mts.rs>
2018-09-20 01:09:09 +0200

    updates

--- src/component/view/notification/Notification.js
+++ src/component/view/notification/Notification.js
@@ -18,9 +18,9 @@ const notifications = [
     {
         id: 5453,
         type: 'Promotion',
-        image: '    http://www.absolat.com/images/promotion-in-marketing.jpg',
+        image: 'http://www.absolat.com/images/promotion-in-marketing.jpg',
         title: '%30 off on sports betting',
-        link: '    https://www.google.com/',
+        link: 'https://www.google.com/',
     },
     {
         id: 5236,
@@ -32,21 +32,30 @@ const notifications = [
 
 class Notification extends Component {
     state = {
-        itemArray: {}
+        itemArray: {},
+        newArray: []
     };
 
     handleAddProject = (x) => {
-        this.setState({itemArray: x[0]});
+        this.setState({itemArray: x[0], newArray: x});
         notifications.shift();
     };
 
     render() {
         return (
-            <div className="Overview">
-                <button onClick={() => this.handleAddProject(notifications)}>New Project</button>
+            <div className="overview">
+                        <div className="circle"><p className="number">{this.state.newArray.length}</p></div>
+                <div className="btn" onClick={() => this.handleAddProject(notifications)}></div>
+                <div className="top"><p>NOTIFICATIONS</p></div>
                 {this.state.itemArray ?
-                    <div>
-                        <p>{this.state.itemArray.id}</p>
+                    <div className="container">
+                        
+                        <div><p className="title">{this.state.itemArray.title}</p></div>
+                        <div><p className="text">{this.state.itemArray.text}</p></div>
+                        <div><p className="requirement">{this.state.itemArray.requirement}</p></div>
+                        <div><img className="img" alt="promo" src={this.state.itemArray.image} img/></div>
+                        <div><a href={this.state.itemArray.link}>Click here</a></div>
+
                     </div> : <p>{"There is no more notification"}</p>
                 }
             </div>
